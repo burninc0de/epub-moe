a241c924a92dd411620c141f3aaa0bd58e9b7264
AK <ariberlin@gmail.com>
2026-08-14 08:48:06 +0300

    fix: resizer bars style

--- src/components/Resizer.tsx
+++ src/components/Resizer.tsx
@@ -7,13 +7,13 @@ interface ResizerProps {
 
 export const Resizer: React.FC<ResizerProps> = ({ onMouseDown, direction = 'horizontal' }) => {
     const directionClasses = direction === 'horizontal'
-        ? "w-1.5 h-full cursor-col-resize"
-        : "h-1.5 w-full cursor-ns-resize";
+        ? "w-1.5 h-full cursor-col-resize -mx-[3px]"
+        : "h-1.5 w-full cursor-ns-resize -my-[3px]";
 
     return (
         <div
             onMouseDown={onMouseDown}
-            className={`${directionClasses} flex-shrink-0 bg-transparent hover:bg-blue-500/50 transition-colors duration-150`}
+            className={`${directionClasses} relative z-20 flex-shrink-0 bg-transparent hover:bg-blue-500 transition-colors duration-150`}
         />
     );
 };
