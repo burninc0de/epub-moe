23d89beaf356e62dd0bc4eca4ea911c2d06b86f6
AK <ariberlin@gmail.com>
2026-08-14 08:27:57 +0300

    fix: scrollbar under title bar

--- src/App.tsx
+++ src/App.tsx
@@ -430,8 +430,7 @@ const App: React.FC = () => {
 
         {/* Middle Column (ContentViewer) */}
         <div className="flex-1 flex flex-col min-w-0">
-          <div className="flex-1 overflow-y-auto">
-            <ContentViewer
+          <ContentViewer
               chapter={currentChapter}
               fragments={fragments}
               selectedFragment={selectedFragment}
@@ -466,7 +465,6 @@ const App: React.FC = () => {
               fragmentSpacing={fragmentSpacing}
               codeThemeId={codeThemeId}
             />
-          </div>
         </div>
 
         {!isRightPanelCollapsed && (
